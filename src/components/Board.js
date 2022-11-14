8ae6369b6b7a5ccf1b926dc442db8d5ee6fb590b
Maria Sos <88471593+Maria-Sos@users.noreply.github.com>
2022-11-14 17:41:02 -0600

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,6 @@
 import Card from "./Card";
 
 function Board (props) {
-console.log(props.field)
   return(
 	<div className='board'>
 	  {props.field.map(el => <Card card={el} key={el.id} openCard={props.openCard}/>)}
